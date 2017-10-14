569d20c592d7dfd63c58b8b7a18db32e64e453f4
Tim Urista <tim.urista@healthtrio.com>
2017-10-14 16:09:14 -0700

    master update server.js in server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
+
 // middleware
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
@@ -10,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 const currentYear = new Date().getFullYear();
 
 app.get('/', (req, res) => {
-  res.render('about.hbs', {
+  res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to the website',
     currentYear
