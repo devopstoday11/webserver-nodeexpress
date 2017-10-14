39b59f7c9b1b8880d411f1fa096e0cade512ff6f
Tim Urista <tim.urista@healthtrio.com>
2017-10-14 15:32:33 -0700

    master update package.json in package.json??

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+// middleware
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.send({
+    name: 'Tim',
+    likes: [
+      'soccer',
+      'cities'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({info: 'bad request'});
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000')
+});
+
