049274b734f395fe7c13cb6d67198bd91c266e4e
Tim Urista <tim.urista@healthtrio.com>
2017-10-17 06:27:13 -0700

    added test for get todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,16 @@ const request = require('supertest');
 const {app} = require('../server-mongo');
 const {Todo} = require('../models/todo');
 
+const todos = [
+  {text: "first test todo"},
+  {text: "second test todo"}
+];
+
 beforeEach(done => {
   // remove it all
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 })
 
 describe('POST /todos', () => {
@@ -25,7 +32,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then(todos => {
+        Todo.find({text}).then(todos => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -46,9 +53,22 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.find().then(todos => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(todos.length);
           done();
         }).catch(e => done(e));
       })
   })
 })
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(todos.length);
+      })
+      .end(done);
+  });
+});
+
