ea1045a659ec97aa18e334d9c8e105c1a312acab
Tim Urista <timothy.urista@gmail.com>
2017-10-27 06:17:45 -0700

    mustache js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,21 +9,27 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('new message', message);
+  var template = jQuery('#message-template').html();
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
-});
+
+  var html = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
+ });
 
 socket.on('newLocationMessage', function(message) {
   console.log('new location message', message);
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My Current location</a>');
   var formattedTime = moment(message.createdAt).format('h:mm a');  
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-location-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 // jquery
